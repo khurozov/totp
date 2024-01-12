c4894aad4ae1df2bfa7bc2357e3327b719291248
Ilyos Khurozov <ilyos@khurozov.uz>
2024-01-12 21:01:15 +0500

    now period uses seconds

--- src/main/java/uz/khurozov/totp/TOTP.java
+++ src/main/java/uz/khurozov/totp/TOTP.java
@@ -1,30 +1,30 @@
 package uz.khurozov.totp;
 
 public class TOTP extends HOTP {
-    private final long period;
+    private final int period;
 
-    public static final long DEFAULT_PERIOD = 30_000L;
+    public static final int DEFAULT_PERIOD = 30;
 
     public TOTP(String secret) {
         super(secret);
         this.period = DEFAULT_PERIOD;
     }
 
-    public TOTP(Algorithm algorithm, String secret, int digits, long period) {
+    public TOTP(Algorithm algorithm, String secret, int digits, int period) {
         super(algorithm, secret, digits);
         this.period = period;
     }
 
     @Override
-    public String getCode(long millis) {
-        return super.getCode(millis / period);
+    public String getCode(long second) {
+        return super.getCode(second / period);
     }
 
     public String getCode() {
-        return this.getCode(System.currentTimeMillis());
+        return this.getCode(System.currentTimeMillis() / 1000);
     }
 
-    public long getPeriod() {
+    public int getPeriod() {
         return period;
     }
 }
